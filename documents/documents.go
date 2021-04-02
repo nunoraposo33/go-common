adffd23eb04f16b17aeb54697f41bc5f85c578e5
nunoraposo33 <42452798+nunoraposo33@users.noreply.github.com>
2021-04-02 22:34:53 +0000

    Add Document struct.

---
+++ documents/documents.go
@@ -0,0 +1,33 @@
+package edocuments
+
+type DocumentFetchQuery struct {
+	Type   string `json:"type"`
+	Series string `json:"series"`
+	Number string `json:"number"`
+}
+
+type Document struct {
+	Type             string  `json:"type"` // FT ; FR ; FS  ; DI  ;
+	Serie            string  `json:"serie"`
+	Number           string  `json:"number"`
+	UserId           int     `json:"userId"`
+	Observations     string  `json:"observations"`
+	ClientId         string  `json:"clientId"`
+	ClientName       string  `json:"clientName"`
+	ClientAddress    string  `json:"clientAddress"`
+	ClientVat        string  `json:"clientVat"`
+	ClientPostalCode string  `json:"clientPostalCode"`
+	ClientCity       string  `json:"clientCity"`
+	UniqueID         string  `json:"uniqueID"`
+	Total            float32 `json:"total"`
+	SubTotal         float32 `json:"subTotal"`
+	Rows             []struct {
+		ItemKeyId       string  `json:"itemKeyId"`
+		ItemDescription string  `json:"itemDescription"`
+		Quantity        string  `json:"quantity"`
+		UnitPrice       float32 `json:"unitPrice"`
+		IncludesTaxes   bool    `json:"includesTaxes"`
+		TaxType         string  `json:"TaxType"` // ISE ; NOR  ; RED  ; INT
+		TaxValue        float32 `json:"taxValue"`
+	}
+}
--- eclients/documents.go
+++
@@ -1,7 +0,0 @@
-package eclients
-
-type DocumentFetchQuery struct {
-	Type   string `json:"type"`
-	Series string `json:"series"`
-	Number string `json:"number"`
-}
